17e2e83c33dcb31d557e6fecf21920d7e11b2f52
Harry Pfleger <harry@ardimedia.com>
2017-01-24 17:26:33 +0100

    Clean code.

---
+++ angular-portal-azure/npm-update.bat
@@ -0,0 +1,18 @@
+﻿npm install --save @angular/common@2.4.4
+npm install --save @angular/compiler@2.4.4
+npm install --save @angular/compiler-cli@2.4.4
+npm install --save @angular/core@2.4.4
+npm install --save @angular/forms@2.4.4
+npm install --save @angular/http@2.4.4
+npm install --save @angular/platform-browser@2.4.4
+npm install --save @angular/platform-browser-dynamic@2.4.4
+npm install --save @angular/platform-server@2.4.4
+npm install --save @angular/router@3.4.4
+npm install --save @angular/upgrade@2.4.4
+npm install --save-dev @types/angular@1.6.2
+npm install --save-dev @types/node@7.0.3
+
+
+npm install --save-dev jasmine@2.5.3
+npm install --save-dev karma-webpack@2.0.1
+npm install --save-dev typescript@2.1.5
